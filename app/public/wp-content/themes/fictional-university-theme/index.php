30abdeb056efa57a2e2c447f344ac5f11258283e
Andy <andrei_achim@outlook.com>
2023-07-24 20:20:35 +0300

    first commit

---
+++ app/public/wp-content/themes/fictional-university-theme/index.php
@@ -0,0 +1,12 @@
+<?php
+
+    $names = array('Andy', 'Grasu', 'Grasa', 'Ronaldo');
+    $count = 0;
+    while($count < count($names)) {
+        echo "<li>Hi, my name is $names[$count]</li>";
+        $count++;
+    }
+
+?>
+
+
